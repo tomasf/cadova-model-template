82dd6c8133b752f27863ea542d65ac432e9a30dd
Tomas Franzén <tomas@tomasf.se>
2026-09-15 10:46:02 +0200

    Update Cadova dependency version to 0.10.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "model",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/tomasf/Cadova.git", .upToNextMinor(from: "0.9.0")),
+        .package(url: "https://github.com/tomasf/Cadova.git", .upToNextMinor(from: "0.10.0")),
     ],
     targets: [
         .executableTarget(
